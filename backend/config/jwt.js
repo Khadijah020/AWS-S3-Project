253244760f67f7d2d9ca267dfe4225c118b9ea10
Khadijah Hasan <khadijahzaheer19@gmail.com>
2025-01-21 18:19:27 +0500

    final commit

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -1,1 +1,18 @@
 //JWTconfiguration
+const jwt = require('jsonwebtoken');
+
+const generateToken = (user) => {
+    return jwt.sign(
+        {
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '1h' }
+    );
+};
+
+module.exports = { generateToken };
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,22 @@
-# User schema and model
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please enter username"],
+    },
+    email: {
+        type: String,
+        required: [true, "Enter Email Address"],
+        unique: [true, "Email address already registered!"]
+    },
+    password: {
+        type: String,
+        required: [true, "Enter password"],
+    },
+
+}, {
+    timestamps: true,
+})
+
+module.exports = mongoose.model("User", userSchema)
