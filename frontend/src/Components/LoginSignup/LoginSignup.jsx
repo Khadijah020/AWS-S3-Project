3ac13173ba67450d688dbd02a6acf5f4ee75bfbd
Khadijah Hasan <khadijahzaheer19@gmail.com>
2025-01-23 01:21:11 +0500

    extras

--- frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -46,60 +46,48 @@ const LoginSignup = ({ onLogin }) => {
     }
   };
 
-  return (
-    <div className='container'>
-      <div className='header'>
-        <div className='text'>{action}</div>
-        <div className='underline'></div>
-      </div>
-      <div className='inputs'>
-        {action === "Login" ? null : (
-          <div className='input'>
-            <img src={userIcon} alt="" />
-            <input
-              type="text"
-              placeholder='Name'
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
-          </div>
-        )}
-        <div className='input'>
-          <img src={emailIcon} alt="" />
-          <input
-            type="email"
-            placeholder='Email ID'
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
+    return (
+        <div className='container'>
+            <div className='header'>
+                <div className='text'>{action}</div>
+                <div className='underline'></div>
+            </div>
+            <div className='inputs'>
+
+                {
+                    action === "Login" ? <div></div> : <div className='input'>
+                        <img src={userIcon} alt="" />
+                        <input type="text" placeholder='Name' value={name} onChange={(event) => setName(event.target.value)} />
+                    </div>
+                }
+
+                <div className='input'>
+                    <img src={emailIcon} alt="" />
+                    <input type="email" placeholder='Email ID' value={email} onChange={(event) => setEmail(event.target.value)} />
+                </div>
+
+                <div className='input'>
+                    <img src={passwordIcon} alt="" />
+                    <input type="password" placeholder='Password' value={password} onChange={(event) => setPassword(event.target.value)} />
+                </div>
+            </div>
+            {action === "Sign Up" ? <div></div> : <div className="forgot-pswd">Forgot Password? <span>Click Here!</span></div>}
+            <div className="submit-container">
+                {action !== "Login" && (
+                    <div className="submit gray" onClick={() => setAction("Login")}>Login</div>
+                )}
+                {action !== "Sign Up" && (
+                    <div className="submit gray" onClick={() => setAction("Sign Up")}>Sign Up</div>
+                )}
+                <button className="submit" onClick={handleSubmit}>
+                    {action === "Sign Up" ? "Sign Up" : "Login"}
+                </button>
+            </div>
+
+
+            {error && <div className="error-message">{error}</div>}
         </div>
-        <div className='input'>
-          <img src={passwordIcon} alt="" />
-          <input
-            type="password"
-            placeholder='Password'
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-      </div>
-      {action === "Sign Up" ? null : (
-        <div className="forgot-pswd">Forgot Password? <span>Click Here!</span></div>
-      )}
-      <div className="submit-container">
-        {action !== "Login" && (
-          <div className="submit gray" onClick={() => setAction("Login")}>Login</div>
-        )}
-        {action !== "Sign Up" && (
-          <div className="submit gray" onClick={() => setAction("Sign Up")}>Sign Up</div>
-        )}
-        <button className="submit" onClick={handleSubmit}>
-          {action === "Sign Up" ? "Sign Up" : "Login"}
-        </button>
-      </div>
-      {error && <div className="error-message">{error}</div>}
-    </div>
-  );
-};
+    )
+}
 
 export default LoginSignup;
